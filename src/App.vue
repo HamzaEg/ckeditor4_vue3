f34686aa97df1343130937fbd5a78d27b61da58b
HamZa <h.egnaidi@gmail.com>
2023-01-18 19:58:25 +0100

    filtering options

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template>
   <v-layout>
-    <v-app-bar color="grey-lighten-2">
+    <v-app-bar color="light-blue-darken-2">
       <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
     </v-app-bar>
-    <v-navigation-drawer color="grey-darken-2" permanent v-model="drawer">
+    <v-navigation-drawer color="light-blue-lighten-5" permanent v-model="drawer">
 
       <v-card class="mx-auto" max-width="300">
         <v-list density="compact">
 
-          <v-list-item v-for="(item, i) in navItems" :key="i" :value="item" active-color="primary"
+          <v-list-item v-for="(item, i) in navItems" :key="i" :value="item" active-color="blue-darken-3"
             @click="selectNav(i, item)">
             <template v-slot:prepend>
               <v-icon :icon="item.icon"></v-icon>
@@ -22,12 +22,12 @@
 
     </v-navigation-drawer>
     <v-main>
-      <v-card height="3em" class="d-flex justify-center bg-purple-lighten-3">
+      <v-card height="3em" class="d-flex justify-center bg-light-blue-lighten-5">
         <template v-slot:title>
        {{ navTitle }}
     </template>
       </v-card>
-      <DokumentenManagement v-show="isDocManag"/>
+      <DokumentenManagement v-if="isDocManag"/>
       
     </v-main>
   </v-layout>
@@ -46,23 +46,23 @@ export default {
   },
 
   setup() {
+    const drawer = ref(false);
     const isDocManag = ref(false);
     const navTitle = ref("");
     function selectNav(i, navItem) {
       i === 1 ? isDocManag.value = true : isDocManag.value = false;
-      navTitle.value = navItem.text
-      console.log(i);
-
+      navTitle.value = navItem.text;
+      drawer.value = !drawer.value;
     }
 
 
 
-    return { isDocManag, selectNav, navTitle }
+    return { isDocManag, selectNav, navTitle, drawer }
 
   },
 
   data: () => ({
-    drawer: false,
+    
     navItems: [
       { text: 'Nursit Institute', icon: 'mdi-home' },
       { text: 'Dokumentverwaltung', icon: 'mdi-file-document-edit-outline' }
--- src/components/DokumentenManagement.vue
+++ src/components/DokumentenManagement.vue
@@ -1,7 +1,7 @@
 <template>
 
-    <v-row no-gutters class="bg-grey">
-        <v-col cols="3">
+    <v-row no-gutters class="bg-grey-lighten-1 d-flex justify-space-end">
+        <v-col cols="3" style="max-width: 320px;">
             <v-sheet class="pa-2 ma-2">
                 <v-card class="mx-auto" max-width="344" variant="outlined">
                     <v-card-item>
@@ -9,11 +9,17 @@
                             <div class="text-overline ma-0">
                                 Belegung
                             </div>
-
-                            <v-select density="compact" label="Abteilung" :items="['Neurologie']"></v-select>
-                            <v-select density="compact" label="Zweig" :items="['ALLe']"></v-select>
-                            <v-select density="compact" label="Station"
-                                :items="['Ebene1', 'Ebene2', 'Ebene3', 'Ebene4', 'Ebene5', 'Ebene6']"></v-select>
+                            <!-- :hint="`${selectedCategorie.name}, ${selectedCategorie.abbr}`"  -->
+                            <v-select v-model="selectedAbteilung" :items="abteilungs" :hint="selectedAbteilung.abbr"
+                                density="compact" item-title="name" item-value="abbr" label="Select" persistent-hint
+                                return-object single-line>
+                            </v-select>
+                            <v-select v-model="selectedZweig" :items="zweigs" density="compact" item-title="name"
+                                item-value="abbr" label="Select" persistent-hint return-object single-line>
+                            </v-select>
+                            <v-select v-model="selectedStation" :items="stations" density="compact" item-title="name"
+                                item-value="abbr" label="Select" persistent-hint return-object single-line>
+                            </v-select>
 
                         </div>
                     </v-card-item>
@@ -26,9 +32,11 @@
                             <div class="text-overline ma-0">
                                 Patientendaten
                             </div>
-                            <v-text-field density="compact" label="Name"></v-text-field>
-                            <v-text-field density="compact" label="Vorname"></v-text-field>
-                            <v-text-field density="compact" label="Geb. Datum"></v-text-field>
+                            <v-text-field density="compact" label="Name" v-model="selectdPatient.name"></v-text-field>
+                            <v-text-field density="compact" label="Vorname"
+                                v-model="selectdPatient.firstName"></v-text-field>
+                            <v-text-field density="compact" label="Geb. Datum"
+                                v-model="selectdPatient.birthDay"></v-text-field>
                         </div>
                     </v-card-item>
 
@@ -40,20 +48,25 @@
                             <div class="text-overline ma-0">
                                 Dokumente
                             </div>
-                            <v-select density="compact" label="Zeitraum" :items="['Letzten Monat']"></v-select>
+                            <v-select v-model="selectedZeitraum" :items="zeitraums"
+                                :hint="'ab: ' + selectedZeitraum.abbr" density="compact" item-title="name"
+                                item-value="abbr" label="Zeitraum" persistent-hint return-object single-line>
+                            </v-select>
+
                             <v-card class="mx-auto" max-width="344" variant="outlined">
                                 <v-card-item>
                                     <div>
                                         <div class="text-overline ma-0">
-                                            Arztbrief
+                                            Arztbrief {{  radios}}
                                         </div>
-                                        <v-radio-group>
-                                            <v-radio density="compact" label="keine" value="1"></v-radio>
-                                            <v-radio density="compact" label="angelegte Arztbriefe" value="2"></v-radio>
+                                        <v-radio-group v-model="radios">
+
+                                            <v-radio density="compact" label="keine" value="keine"></v-radio>
+                                            <v-radio density="compact" label="angelegte Arztbriefe" value="angelegt"></v-radio>
                                             <v-radio density="compact" label="versendete nicht fixierte Arztbriefe"
                                                 value="3"></v-radio>
-                                            <v-radio density="compact" label="fixierte Arztbriefe" value="4"></v-radio>
-                                            <v-radio density="compact" label="alle" value="5"></v-radio>
+                                            <v-radio density="compact" label="fixierte Arztbriefe" value="fixierte"></v-radio>
+                                            <v-radio density="compact" label="alle" value=""></v-radio>
                                         </v-radio-group>
                                     </div>
                                 </v-card-item>
@@ -68,17 +81,12 @@
             </v-sheet>
         </v-col>
 
-        <v-col cols="9">
-            <v-data-table 
-            v-model:items-per-page="itemsPerPage" 
-            :headers="headers" 
-            :items="documents" 
-            item-value="name"
-            class="elevation-1"
-            @click:row="selectVorlage"
-            >
+        <v-col cols="9" style="max-width: 100;" class="pt-2">
+
+            <v-data-table v-model:items-per-page="itemsPerPage" :headers="headers" :items="filteredDocuments"
+                item-value="name" class="elevation-1" @click:row="selectVorlage">
             </v-data-table>
-            <div class="mb-6 bg-purple-lighten-5">
+            <div class="mb-6 bg-light-blue-lighten-5">
                 <v-container fluid>
                     <v-row class="d-flex justify-center ">
                         <v-col cols="12" sm="3" md="3">
@@ -107,44 +115,118 @@ export default {
         // CkEditor4
     },
     setup() {
-        const  documents =ref([
-                { dokument: "Ergänzender Enthlassungsbrief Wf", v: "", erstelldatum: "26.09.2022", patient: "Hering, Detlef - 08.10.1949", vorlage: "Brief", Belegung: "Neuro-Akut-Ebene1", zustand: "angelegt", uDatum: "16.06.2022", uStatus: "Neu", unterschrift1: "Simona-Gabri...", unterschrift2: "", unterschrift3: "" },
-                { dokument: "Notw. für Begleitperson", v: "", erstelldatum: "13.09.2022", patient: "Hering, Detlef - 08.10.1949", vorlage: "Brief", Belegung: "Neuro-Akut-Ebene1", zustand: "angelegt", uDatum: "16.06.2022", uStatus: "Zur Unterschrift 1", unterschrift1: "Prof. Dr. me....", unterschrift2: "Simona-Gabri...", unterschrift3: "" },
-                { dokument: "Aktuelle Medikation", v: "", erstelldatum: "14.09.2022", patient: "Raida, Klaus - 01.11.1955", vorlage: "Brief", Belegung: "Neuro-Akut-Ebene1", zustand: "angelegt", uDatum: "16.06.2022", uStatus: "Neu", unterschrift1: "M. Canelo Är...", unterschrift2: "", unterschrift3: "" },
-                { dokument: "Ergänzender Entlassungsbrief WF", v: "", erstelldatum: "06.10.2022", patient: "Schätti, Thomas - 04.09.1958", vorlage: "Brief", Belegung: "Neuro-Akut-Ebene1", zustand: "angelegt", uDatum: "16.06.2022", uStatus: "Zur Formatierung", unterschrift1: "Simona-Gabri...", unterschrift2: "", unterschrift3: "" },
-                {
-                    dokument: "Liegebescheinigung mit Diagnosen", v: "", erstelldatum: "14.09.2022",
-                    patient: "Roede, Heiko - 16.11.1962", vorlage: "Brief", Belegung: "Neuro-Akut-Ebene1",
-                    zustand: "angelegt", uDatum: "16.06.2022", uStatus: "Neu", unterschrift1: "",
-                    unterschrift2: "Simona-Gabri...", unterschrift3: ""
-                },
-            ])
+        const radios = ref('');
+        
+        const selectdPatient = ref({ name: '', firstName: '', birthDay: '' })
+        const selectedAbteilung = ref({ name: 'Alle', abbr: '' });
+        const abteilungs = ref([
+            { name: 'Alle', abbr: '' },
+            { name: 'Neurologie', abbr: 'Neur' },
+            { name: 'Radiologie', abbr: 'Radi' },
+            { name: 'Gastroenterologie', abbr: 'Gast' },
+        ]);
+
+        const selectedZweig = ref({ name: 'Alle', abbr: '' });
+        const zweigs = ref([
+            { name: 'Alle', abbr: '' },
+            { name: 'Akut', abbr: 'Akut' },
+            { name: 'Notaufnahmen', abbr: 'Not' },
+
+        ]);
+
+        const selectedStation = ref({ name: 'Alle', abbr: '' });
+        const stations = ref([
+            { name: 'Alle', abbr: '' },
+            { name: 'Ebene1', abbr: 'Ebene1' },
+            { name: 'Ebene2', abbr: 'Ebene2' },
+            { name: 'Ebene3', abbr: 'Ebene3' },
+            { name: 'Ebene4', abbr: 'Ebene4' },
+            { name: 'Ebene5', abbr: 'Ebene5' },
+            { name: 'Ebene6', abbr: 'Ebene6' },
+        ]);
+
+        const selectedZeitraum = ref({ name: 'Alle', abbr: '' });
+        const zeitraums = ref([
+            { name: 'Alle', abbr: '' },
+            { name: 'Letzten Monat', abbr: subtractMonths(new Date(), 1) },
+            { name: 'Letzten Quartal', abbr: subtractMonths(new Date(), 3) },
+            { name: 'Letzten Habjahr', abbr: subtractMonths(new Date(), 6) },
+            { name: 'Letzten Jahr', abbr: subtractMonths(new Date(), 12) },
+        ]);
+
+
+
+        const filteredDocuments = ref([]);
+        const documents = ref([
+            { dokument: "Ergänzender Enthlassungsbrief Wf", v: "", erstelldatum: "26.09.2022", patient: "Hering, Detlef - 08.10.1949", vorlage: "Brief", Belegung: "Neuro-Akut-Ebene1", zustand: "angelegt", uDatum: "16.06.2022", uStatus: "Neu", unterschrift1: "Simona-Gabri...", unterschrift2: "", unterschrift3: "" },
+            { dokument: "Notw. für Begleitperson", v: "", erstelldatum: "13.09.2022", patient: "Hering, Detlef - 08.10.1949", vorlage: "Brief", Belegung: "Neuro-Akut-Ebene1", zustand: "angelegt", uDatum: "16.06.2022", uStatus: "Zur Unterschrift 1", unterschrift1: "Prof. Dr. me....", unterschrift2: "Simona-Gabri...", unterschrift3: "" },
+            { dokument: "Aktuelle Medikation", v: "", erstelldatum: "14.09.2022", patient: "Raida, Klaus - 01.11.1955", vorlage: "Brief", Belegung: "Neuro-Akut-Ebene1", zustand: "angelegt", uDatum: "16.06.2022", uStatus: "Neu", unterschrift1: "M. Canelo Är...", unterschrift2: "", unterschrift3: "" },
+            { dokument: "Ergänzender Entlassungsbrief WF", v: "", erstelldatum: "06.10.2022", patient: "Schätti, Thomas - 04.09.1958", vorlage: "Brief", Belegung: "Neuro-Akut-Ebene1", zustand: "angelegt", uDatum: "16.06.2022", uStatus: "Zur Formatierung", unterschrift1: "Simona-Gabri...", unterschrift2: "", unterschrift3: "" },
+            {
+                dokument: "Liegebescheinigung mit Diagnosen", v: "", erstelldatum: "14.09.2022",
+                patient: "Roede, Heiko - 16.11.1962", vorlage: "Brief", Belegung: "Radi-Not-Ebene5",
+                zustand: "", uDatum: "16.06.2022", uStatus: "Neu", unterschrift1: "",
+                unterschrift2: "Simona-Gabri...", unterschrift3: ""
+            },
+            {
+                dokument: "Liegebescheinigung mit Diagnosen", v: "", erstelldatum: "14.09.2022",
+                patient: "Roede, Heiko - 16.11.1962", vorlage: "Brief", Belegung: "Radi-Akut-Ebene1",
+                zustand: "fixierte", uDatum: "16.06.2022", uStatus: "Neu", unterschrift1: "",
+                unterschrift2: "Simona-Gabri...", unterschrift3: ""
+            },
+        ]);
+
+        function subtractMonths(date, months) {
+            //console.log(date, months);
+            console.log(date, months);
+            
+            date.setMonth(date.getMonth() - months);
+
+            // const formatedDate = date.toLocaleString('de-DE',{ year: "numeric", month: "numeric", day: "numeric"});
+            const formatedDate = date.toLocaleString('en-US',{ year: "numeric", month: "numeric", day: "numeric"});
+            
+
+            return  formatedDate;
+        }
 
 
         function selectVorlage(value) {
-            const tr = value.target.parentNode;
-            console.log(tr);
+            // const tr = value.target.parentNode;
+            // console.log(tr);
             console.log("selectVorlage", value);
         }
         function searchDocuments() {
-            
-            documents.value = documents.value.filter(a => a.dokument.includes('zender'))
-
-            //console.log(documents);
-            
-            console.log('search docus');
-            console.log(documents);
+            console.log(new Date(selectedZeitraum.value.abbr) - new Date());
+            filteredDocuments.value = documents.value.filter(a => 
+            a.Belegung.includes(selectedAbteilung.value.abbr) &&
+            a.Belegung.includes(selectedZweig.value.abbr) &&
+            a.Belegung.includes(selectedStation.value.abbr) &&
+            a.patient.includes(selectdPatient.value.name) &&
+            a.patient.includes(selectdPatient.value.firstName) &&
+            a.patient.includes(selectdPatient.value.birthDay) 
+            //&& compareDates(a.erstelldatum, selectedZeitraum.value.abbr)
+            && (radios.value === 'keine') ? a.zustand === '' : a.zustand.includes(radios.value)
+            );
         }
+        // function compareDates(date1, date2) {
+            
+        //     console.log(typeof(date1), Date.parse(date1));
+        //     console.log(typeof(date2), typeof(Date.parse(date2)));
+
+        //     const date1formated = subtractMonths(subtractMonths, 0)
+        //     console.log(new Date(date1formated));
+        //     return true         
+        // }
 
 
-        return { selectVorlage, searchDocuments, documents }
+        return { subtractMonths, selectVorlage, searchDocuments, documents, abteilungs, selectedAbteilung, filteredDocuments, zweigs, selectedZweig, stations, selectedStation, selectdPatient, zeitraums, selectedZeitraum, radios }
 
     },
     data() {
         return {
             isFiltered: false,
             isUmlauf: false,
-           
+            
             itemsPerPage: 10,
             headers: [
                 {
@@ -167,6 +249,8 @@ export default {
                 { title: 'unterschrift3', align: 'start', key: 'unterschrift3', width: '10%' },
 
             ],
+
+
 
         }
     },
